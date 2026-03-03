86714f01da29b0d818ff00996a86e19f841c6f71
Jiyansh2009-Rana <ranamayuri832001@gmail.com>
2026-03-03 14:37:40 +0530

    add file

---
+++ Frontend Ui .py
@@ -0,0 +1,69 @@
+import streamlit as st
+import requests
+
+
+Base_url = "https://rag-backend-gamma.vercel.app"
+
+st.set_page_config (page_title = "Rag Based llm for (pdf Data)",layout = "wide")
+
+
+st.title("📋 Rag Based llm and Upload (Pdf Data) 📝")
+st.markdown ("Power By Groq llm As a llm ,,  Jina-embeddings-v3 As a Embedding  Model And SupaBase As a Vector DB")
+
+
+with st.sidebar:
+    st.header ("Upload Data In format 'Pdf' ")
+    uploaded_file = st.file_uploader ("Choose your Pdf Data", type = "pdf")
+    
+    if st.button ("upload your Data"):
+        if uploaded_file is not None:
+            with st.spinner ("proceesing pdf..."):
+                files = {
+                            "file" : (uploaded_file.name, uploaded_file.getvalue(), "application/pdf")
+                            }
+                           
+                response = requests.post(f"{Base_url}/upload", files = files)
+                
+                if response.status_code == 200:
+                    st.success(response.json().get("message"))
+                else:
+                    st.error(f"Error: {response.text}")
+        else:
+            st.warning("Please select a file first.")
+
+
+st.header("💬 Ask Your Qusention Related Data And Other what you want 🌐")
+
+sys_prompt = st.text_area("Enter your System Prompt Here (What Should be  Work LLM ? ) ")
+
+user_query = st.text_input ("Enter your Question Here")
+
+if st.button("Ask Ai"):
+    if not user_qurey:
+        st.warning(" Please Enter Your Question")
+    else:
+        with st.spinner("Searching and generating answer.."):
+            
+            Question = {
+                "system_prompt": sys_prompt,
+                "user_query": user_query
+            }
+            
+            response = requests.post(f"{Base_url}/query",data = Question )
+            
+            if response.status_code == 200:
+                result = response.json()
+                
+                st.subheader("Answer:")
+                st.write(result.get("answer"))
+                
+                
+                with st.expander("View Related Content "):
+                    related = result.get("sources", [])
+                    if related :
+                        for related in set(related):
+                            st.info(f"Related Content : {related}")
+                    else :
+                        st.write("No Specific content Found ")
+            else :
+                st.error(f"Error {response.text}")
